e53e1a725a1d267e432b44b42913f2cffddf463c
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-09-16 01:57:29 +0200

    snake is now controllable

--- src/serpent.adb
+++ src/serpent.adb
@@ -2,6 +2,7 @@ with Terminal_Interface.Curses;
 use  Terminal_Interface.Curses;
 with Terminal_Interface.Curses.Text_IO;
 use  Terminal_Interface.Curses.Text_IO;
+with Terminal_Interface.Curses.Text_IO.Integer_IO;
 
 with Ada.Containers.Doubly_Linked_Lists ;
 with Ada.Calendar; use Ada.Calendar;
@@ -10,13 +11,31 @@ procedure serpent is
    type T_Position is record
       line : Line_Position; 
       column : Column_Position ; 
-   end record ;
+   end record;
+   function "+" (Left, Right : T_Position) return T_Position is
+      res : T_Position;
+   begin
+      res.line := left.line + right.line;
+      res.column := left.column + right.column;
+      return res;
+   end "+";
    package P_Lists is new Ada.Containers.Doubly_Linked_Lists(T_Position) ; 
    use P_Lists ;
+   package Line_Position_Text_IO
+   is new Terminal_Interface.Curses.Text_IO.Integer_IO(Line_Position);
+   use Line_Position_Text_IO;
+   package Column_Position_Text_IO 
+   is new Terminal_Interface.Curses.Text_IO.Integer_IO(Column_Position);
+   use Column_Position_Text_IO;
+
+
+   type T_Direction is (Up, Down, Left, Right);
 
    snake : list;
+   direction : T_Direction := Right;
    snake_cursor : cursor;
    pos, pop : T_position;
+   delta_pos : T_position :=(0,1);
    temps : Time := Clock; 
    duree : Duration := 1.0;
    key : Real_Key_Code;
@@ -25,6 +44,8 @@ begin
    Init_Screen;
    Set_Timeout_Mode(mode=>Non_Blocking,Amount=>0);
    Set_Cursor_Visibility(curs_visibility);
+   Set_Echo_Mode(SwitchOn => false);
+   Set_KeyPad_Mode(SwitchOn => true);
    border(standard_window);
    Move_cursor(standard_window,0,1);
    Put("jeu du serpent");
@@ -37,10 +58,29 @@ begin
    --star moving the snake
    temps := Clock;
    main: loop
+      declare
+         tmp_delta_pos : T_Position:= (0,0);
+      begin
+         key := Get_Keystroke;
+         case key is
+            when KEY_UP => tmp_delta_pos := (-1,0);--Put("key_up");
+            when KEY_DOWN => tmp_delta_pos := (1,0);--Put("key_down");
+            when KEY_LEFT => tmp_delta_pos := (0,-1);--Put("key_left");
+            when KEY_RIGHT => tmp_delta_pos := (0,1);--Put("key_right");
+            when others => null; 
+         end case;
+         Move_Cursor(Standard_Window,0,15);
+         if tmp_delta_pos /= (0,0) then
+            delta_pos := tmp_delta_pos;
+         end if;
+      end;
       if (Clock - temps) >= 0.5 then
          temps := Clock;
-         pos.column := pos.column + 1;
+         pos := pos + delta_pos;
          exit when pos.column > (Columns - 1);
+         exit when pos.column < (1);
+         exit when pos.line > (Lines - 1);
+         exit when pos.line < (1);
          --add element to the front
          Prepend(snake,pos);
          Add(Line=>pos.line, Column=>pos.column, ch=>'O');
